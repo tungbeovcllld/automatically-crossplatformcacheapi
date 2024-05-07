2c8ca308a4ff4d5fac02a48c7fbbb402e9dcd05a
tungbeovcllld <connorp203pun0suy@hotmail.com>
2024-05-08 00:36:15 +0700

    hi commit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-function maxDepth(root) {
-  if (!root) return 0;
-  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
-}
+const lcmRecursive = (a, b) => {
+  return (a * b) / gcdRecursive(a, b);
+};
